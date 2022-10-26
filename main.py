cb681cde56a19c962ff76e6a45ac16a1ff32b231
rakesh1268248 <114190126+rakesh1268248@users.noreply.github.com>
2022-10-26 09:16:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,14 @@ def st_ui():
   Button=st.sidebar.button('content Analytics')
   #button=st.sidebar.button('Risk Analytics')
   Enter_text = st.sidebar.text_input("Text to search")
-  df = pd.DataFrame(np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],columns=['lat', 'lon'])
+  df = pd.DataFrame([[  37.7790262 , -122.419906  ],
+       [  37.6535403 , -122.4168664 ],
+       [  29.6330078 ,  -97.98859404],
+       [  38.6920451 ,  -75.4013315 ],
+       [  51.4534963 ,    0.21878742],
+       [  39.4225192 , -111.714358  ],
+       [  52.45777545,   -1.86920994],
+       [  36.5748441 ,  139.2394179 ]],columns=['lat', 'lon'])
   st.map(df)
         
 if __name__ == "__main__":
